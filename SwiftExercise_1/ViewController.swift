122cbbc8d4b2bb87ef330aa9d8bf746fece5bc15
Havi_li <1058695900@qq.com>
2015-06-12 15:19:47 +0800

    Initial Commit

---
+++ SwiftExercise_1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftExercise_1
+//
+//  Created by Havi on 15/6/12.
+//  Copyright (c) 2015年 Havi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
